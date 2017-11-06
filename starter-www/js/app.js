8afa895719c3f89c6a3c5b6fcb6bcaff49f5ad63
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-11-06 02:01:10 -0500

    Add files via upload

--- starter-www/js/app.js
+++ starter-www/js/app.js
@@ -17,15 +17,27 @@
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
-            service.findByName($('.search-first').val(), $('.search-last').val()).done(function (employees) {
-                var l = employees.length;
-                var e;
-                $('.employee-list').empty();
-                for (var i = 0; i < l; i++) {
-                    e = employees[i];
-                    $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-                }
-            });
+        var trimmed_first = ($('.search-first').val()).trim();
+        var trimmed_last = ($('.search-last').val()).trim();
+        var IsLetters = false;
+       // if ((Pattern.matches("[a-z]+", trimmed_first)) || (Pattern.matches("[a-z]+", trimmed_last))) {
+            if (!((trimmed_first.indexOf(' ') >= 0) || (trimmed_last.indexOf(' ') >= 0))) {
+                if ((trimmed_first.length >= 2) || (trimmed_last.length >= 2)) {
+                    service.findByName($('.search-first').val(), $('.search-last').val()).done(function (employees) {
+                        var l = employees.length;
+                        var e;
+                        $('.employee-list').empty();
+                        for (var i = 0; i < l; i++) {
+                            e = employees[i];
+                            $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+                        }
+                        if (($('.search-first').val().length == 0) && ($('.search-last').val().length == 0)) {
+                            $('.employee-list').empty();
+                        }
+                    });
+               // }
+            }
         }
+    }
 
 }());
