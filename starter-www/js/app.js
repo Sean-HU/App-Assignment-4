d71e9761275aaea94332ad9622d56558b118d897
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-11-06 01:01:05 -0500

    Add files via upload

---
+++ starter-www/js/app.js
@@ -0,0 +1,31 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        console.log("Service initialized");
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+    $('.search-first').on('keyup', findByName);
+    $('.search-last').on('keyup', findByName);
+
+    $('.help-btn').on('click', function() {
+        alert("Employee Directory v3.4");
+    });
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+    function findByName() {
+            service.findByName($('.search-first').val(), $('.search-last').val()).done(function (employees) {
+                var l = employees.length;
+                var e;
+                $('.employee-list').empty();
+                for (var i = 0; i < l; i++) {
+                    e = employees[i];
+                    $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+                }
+            });
+        }
+
+}());
